b08798ec5240dedc44a17e1e50bbeb6d4841ee42
Diogo-Bellini <diogocvazbellini@icloud.com>
2025-06-16 19:36:01 -0300

    iniciando projeto

---
+++ src/main/java/br/ufscar/dc/dsw/Projeto2DSW/Projeto2DswApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.Projeto2DSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2DswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto2DswApplication.class, args);
+	}
+
+}
