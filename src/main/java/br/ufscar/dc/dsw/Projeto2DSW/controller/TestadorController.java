1cdaf22d70ee06c8be54ab2116f39bf3ac2a327e
jpmrangel <joaopaulomr96@gmail.com>
2025-06-22 14:19:01 -0300

    fix remove dead code and unused imports

--- src/main/java/br/ufscar/dc/dsw/Projeto2DSW/controller/TestadorController.java
+++ src/main/java/br/ufscar/dc/dsw/Projeto2DSW/controller/TestadorController.java
@@ -1,19 +1,11 @@
 package br.ufscar.dc.dsw.Projeto2DSW.controller;
 
-import br.ufscar.dc.dsw.Projeto2DSW.model.Usuario;
-import br.ufscar.dc.dsw.Projeto2DSW.model.Papel;
-import br.ufscar.dc.dsw.Projeto2DSW.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/testador")
 public class TestadorController {
-
-    @Autowired
-    private UserService service;
 
     @GetMapping("/home")
     public String testadorHomePage() {
--- src/main/java/br/ufscar/dc/dsw/Projeto2DSW/service/UserService.java
+++ src/main/java/br/ufscar/dc/dsw/Projeto2DSW/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.List;
 import java.util.Optional;
