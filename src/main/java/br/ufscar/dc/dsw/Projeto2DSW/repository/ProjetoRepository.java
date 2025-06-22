ff64320ccbffcab502040d029854c3b8ed1b8ebf
Fellipe <luizflb@estudante.ufscar.br>
2025-06-22 16:57:21 -0300

    ordenação e exibição dos projetos como testador

--- src/main/java/br/ufscar/dc/dsw/Projeto2DSW/repository/ProjetoRepository.java
+++ src/main/java/br/ufscar/dc/dsw/Projeto2DSW/repository/ProjetoRepository.java
@@ -1,10 +1,14 @@
 package br.ufscar.dc.dsw.Projeto2DSW.repository;
 
 import br.ufscar.dc.dsw.Projeto2DSW.model.Projeto;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
     List<Projeto> findByUsuarios_IdUsuario(Long idUsuario);
+
+    List<Projeto> findByUsuarios_IdUsuario(Long idUsuario, Sort sort);
+
 }
